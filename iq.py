2e61c10bb57781a4885a841cbeb8dbc4cbad1e55
Mellor <jim.mellor@me.com>
2023-08-26 00:03:33 +0100

    more overlays

--- iq.py
+++ iq.py
@@ -418,12 +418,12 @@ if opt.control=="si570":
 
 run_flag = True                 # set false to suspend for help screen etc.
 info_phase = 1                  # > 0 --> show info overlays
-info_counter = 0
-tloop = 0.
-t_last_data = 0.
-nframe = 0
+info_counter = 1
+tloop = 1.
+t_last_data = 1.
+nframe = 1
 t_frame0 = time.time()
-led_overflow_ct = 0
+led_overflow_ct = 1
 startqueue = True
 while True:
 
