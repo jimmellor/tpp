da61e98f199ac38efafe74b9e0c06cc5cdad3cfb
Mellor <jim.mellor@me.com>
2023-08-28 23:33:09 +0100

    configure for Kenwood TS-590s

--- iq_opt.py
+++ iq_opt.py
@@ -108,7 +108,7 @@ op.set_defaults(
     cpu_load_interval       = 3.0,      # cycle time for CPU monitor thread
     fullscreen              = False,    # Use full screen mode? (if not LCD4)
     hamlib                  = True,     # Using Hamlib? T/F (RPi-False)
-    hamlib_device           = "/dev/tty.SLAB_USBtoUART",   # Device address for Hamlib I/O
+    hamlib_device           = "/dev/ttyUSB0",   # Device address for Hamlib I/O
     hamlib_interval         = 1.0,      # Wait between hamlib freq. checks (secs)    
     hamlib_rigtype          = 2031,     # Kenwood ts-590s
     index                   = -1,       # index of audio device (-1 use default)
