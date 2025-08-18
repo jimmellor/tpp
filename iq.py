0a86c48049de2297d946c0f8917c04c8857d18a9
jim <jim.mellor@me.com>
2025-08-18 23:47:57 +0100

    update text color for frequency display in iq.py

--- iq.py
+++ iq.py
@@ -453,7 +453,7 @@ while True:
     if showfreq:
         # Center it and blit just above 2d display
         ww, hh = lgfont.size(msg)
-        surf_main.blit(lgfont.render(msg, 1, BLACK, BGCOLOR), (0, 0))
+        surf_main.blit(lgfont.render(msg, 1, WHITE, WHITE), (0, 0))
                             #(w_middle + x_spectra - ww/2, y_2d-hh))
 
     # show overflow & underrun indicators (for audio, not rtl)
